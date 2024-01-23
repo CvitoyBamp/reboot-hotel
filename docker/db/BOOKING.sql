5a382729bd2a20dc3326a61334fab266240a5d6e
valentinkalinin <kalinin1997@bk.ru>
2024-01-23 21:26:38 +0300

    add comments to sql

--- docker/db/BOOKING.sql
+++ docker/db/BOOKING.sql
@@ -1,9 +1,9 @@
 CREATE TABLE PUBLIC.BOOKING (
-    ORDER_NUMBER SERIAL PRIMARY KEY,
-    USER_ID INTEGER,
-    ROOM_ID INTEGER,
-    START_DATE DATE NOT NULL,
-    END_DATE DATE NOT NULL,
+    ORDER_NUMBER SERIAL PRIMARY KEY, -- номер заказа (первичный ключ)
+    USER_ID INTEGER, -- внешний ключ из таблицы юзер
+    ROOM_ID INTEGER, -- внешний ключ из таблицы номер
+    START_DATE DATE NOT NULL, -- заезд
+    END_DATE DATE NOT NULL, -- выезд
     CONSTRAINT FK_USER
         FOREIGN KEY (USER_ID)
             REFERENCES PUBLIC.USER(ID),
--- docker/db/ROOM.sql
+++ docker/db/ROOM.sql
@@ -1,9 +1,12 @@
 CREATE TABLE PUBLIC.ROOM (
-    ID SERIAL PRIMARY KEY,
-    NAME VARCHAR(64) NOT NULL,
-    PRICE NUMERIC NOT NULL,
-    PHOTOS TEXT[] NOT NULL,
-    ADDS JSONB
+    ID SERIAL PRIMARY KEY, -- первичный ключ
+    NAME VARCHAR(64) NOT NULL, -- наименование номера
+    PRICE NUMERIC NOT NULL, -- цена за день
+    PHOTOS TEXT[] NOT NULL, -- массив из ссылок на фотографии
+    ADDS JSONB -- доп. приколы в номере {"tv": true, "wifi": true, "fridge": true, "conditioner": true}
 );
+
+--1	Luxury	16000.00	{https://avatars.mds.yandex.net/get-altay/10767865/2a0000018a642d65148c57bdbcb4f918c143/XXXL,https://avatars.mds.yandex.net/get-altay/9849468/2a0000018a642d09f208937739cf5db57402/XXXL}	{"tv": true, "wifi": true, "fridge": true, "conditioner": true}
+--2	Norm	8000.00	{https://avatars.mds.yandex.net/get-altay/8075227/2a000001847ad529702695836ac699a11730/XXXL,https://avatars.mds.yandex.net/get-altay/10156117/2a0000018a7e3124ab0c1f64ff2341285abc/XXXL}	{"tv": true, "wifi": true, "fridge": false, "conditioner": true}
 
 drop table room cascade;
