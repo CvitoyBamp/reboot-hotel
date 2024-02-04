42279f78c54e7f124b24b8553c6d312e3da7fcb6
valentinkalinin <kalinin1997@bk.ru>
2024-02-04 19:55:15 +0300

    add admin

---
+++ src/main/resources/static/js/include_components.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#sidebar').load('../components/sidebar.html');
+    $('#navbar').load('../components/navbar.html');
+    $('#logoutModal').load('../components/logout_modal.html');
+});
