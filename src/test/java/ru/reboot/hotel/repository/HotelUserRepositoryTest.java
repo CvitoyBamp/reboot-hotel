aa1d1ee73c2e70218327575e9b01be223fc1d9b5
valentinkalinin <kalinin1997@bk.ru>
2024-02-14 20:49:54 +0300

    add tests

--- src/test/java/ru/reboot/hotel/repository/HotelUserRepositoryTest.java
+++ src/test/java/ru/reboot/hotel/repository/HotelUserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.reboot.hotel.entity.user.HotelUser;
@@ -15,17 +16,15 @@ import ru.reboot.hotel.repository.user.HotelUserRepository;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureDataJpa
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class HotelUserRepositoryTest {
 
     HotelUser hotelUser;
-
-    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
     HotelUserRepository hotelUserRepository;
@@ -34,7 +33,7 @@ public class HotelUserRepositoryTest {
     TestEntityManager testEntityManager;
     @BeforeEach
     void initHotelUser() {
-        hotelUser = new HotelUser(1000L, "Test", passwordEncoder.encode("123456"), "test@bk.ru", LocalDate.of(1997, 3, 13), "80001001010",1, Collections.EMPTY_LIST);
+        hotelUser = new HotelUser(1000L, "Test", new BCryptPasswordEncoder().encode("123456"), "test@bk.ru", LocalDate.of(1997, 3, 13), "80001001010",1, Collections.EMPTY_LIST);
     }
 
     @AfterEach
@@ -52,7 +51,7 @@ public class HotelUserRepositoryTest {
     @Test
     @DisplayName("No user with such email")
     void cantFindUserByEmail() {
-        assertThat(hotelUserRepository.findCustomUserDetailsByEmail("noExistTest@ya.ru")).withFailMessage("User don't found");
+        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> hotelUserRepository.findCustomUserDetailsByEmail("noExistTest@ya.ru").orElseThrow());
     }
 
     @Test
@@ -61,7 +60,4 @@ public class HotelUserRepositoryTest {
         hotelUserRepository.save(hotelUser);
         assertThat(testEntityManager.find(HotelUser.class, hotelUser.getId())).isEqualTo(hotelUser);
     }
-
-
-
 }
